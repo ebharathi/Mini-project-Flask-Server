68a50d796dd0c5eb5663a70f3ac16e0835bf74cc
ebharathi <elayabharathi10102002@gmail.com>
2023-12-04 22:34:37 +0530

    updated transaction

--- Creditcard.py
+++ Creditcard.py
@@ -1,41 +1,49 @@
-import random #generates random value
+import random
 import shelve
 
-def decryptor(n,m,r,cipher,Accname,key_pair): #code to decrypt
-    t=[m[i]+n[i] for i in range(len(m)-1)]
-    t.append(m[(len(m)-1)]+n[(len(m)-1)])
-    q=(t[0]^t[1])
-    for i in range(len(m)-2):
-        q=(q^t[2+i])
-    return(q)
-def decryption2(name,message):  
+def decryptor(n, m, r, cipher, Accname, key_pair):
+    t = [m[i] + n[i] for i in range(len(m) - 1)]
+    t.append(m[len(m) - 1] + n[len(n) - 1])
+    q = t[0] ^ t[1]
+
+    for i in range(len(m) - 2):
+        q = q ^ t[2 + i]
+
+    return q
+
+def decryption2(name, message):
     try:
         with shelve.open('Ci') as db:
-            r=db['random']
-            cipher=db['cipher']
-            Accname=db['Accname']
-            key_pair=db['key_pair']
-        flag=0
-        print(Accname)
-        print("--->",name) 
-        print(message)
-        loc=Accname[name]
-        print(int(loc))
-        for de in range(0,6):
-            print(message[de],loc+de)
-            print(key_pair[loc+de])
-            temp,temp1=[r[x] for x in list(key_pair[loc+de].keys())],[cipher[x] for x in list(key_pair[loc+de].values())]
-            nws=decryptor(temp,temp1,r,cipher,Accname,key_pair)
-            print(nws)
-            print(flag)
-            if nws==message[de]:
-                print("succeded")
+            r = db['random']
+            cipher = db['cipher']
+            Accname = db['Accname']
+            key_pair = db['key_pair']
+
+        print("Accname:", Accname)
+        print("Name:", name)
+        print("Message:", message)
+
+        loc = Accname[name]
+        print("Location:", loc)
+
+        for de in range(0, 6):
+            print("Index:", loc + de)
+            print("Key Pair:", key_pair[str(loc + de)])
+
+            temp = [r[int(x)] for x in key_pair[str(loc + de)].keys()]
+            temp1 = [cipher[int(x)] for x in key_pair[str(loc + de)].values()]
+
+            decrypted_value = decryptor(temp, temp1, r, cipher, Accname, key_pair)
+            print("Decrypted Value:", decrypted_value)
+
+            if decrypted_value == message[de]:
+                print("Succeeded")
                 continue
             else:
-                flag=1
-        if flag==0:
-            return {"error":"false"}
-        else:            
-            return {"error":"true"}
+                return {"error": "true"}
+
+        return {"error": "false"}
+
     except Exception as e:
-        return {"error":"true"}
+        return {"error": "true"}
+
